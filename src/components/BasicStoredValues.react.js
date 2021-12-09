319a72a91fed47ddfa1473edf509b267be0ca92f
Cameron Bradley <cameron@testingtalks.com.au>
2021-12-09 17:41:31 +1100

    updated stored values

--- src/components/BasicStoredValues.react.js
+++ src/components/BasicStoredValues.react.js
@@ -8,6 +8,8 @@ export class BasicStoredValues extends React.Component {
                 <p data-id="first-value" className="storedValue">Testing Talks</p>
                 <p data-id="second-value" className="storedValue">Testing Talks</p>
                 <p data-id="third-value" className="storedValue">Automation</p>
+                <p data-id="fourth-value" className="storedValue">Testing Talks Conference</p>
+                <p data-id="fifth-value" className="storedValue">Testing Talks Hub</p>
             </>
         );
     }
