d6fa242c7b193db82a7d8eac91e357ebfc92cc8b
tzero86 <eliasmedina86@gmail.com>
2023-04-14 16:50:00 -0300

    Videos and Screenshots with playwright

--- e2e/src/step-definitions/hooks/hooks.ts
+++ e2e/src/step-definitions/hooks/hooks.ts
@@ -2,9 +2,10 @@ import { BeforeAll, Before, After, AfterAll } from '@cucumber/cucumber';
 
 const { chromium } = require('playwright')
 
+
 BeforeAll(async () => {
     global.browser = await chromium.launch({ 
-        headless: false 
+        headless: true 
     })
 })
 
@@ -13,13 +14,25 @@ AfterAll(async () => {
 })
 
 
-Before(async () => {
-    global.context = await global.browser.newContext()
+Before(async (scenario) => {
+    global.context = await global.browser.newContext({
+        recordVideo: {
+            dir: `./reports/videos/${scenario.pickle.name}`,
+        }
+    })
     global.page = await global.context.newPage()
+    await global.page.waitForLoadState()
 })
 
 
-After(async () => {
+After(async (scenario) => {
+    const scenarioStatus = scenario.result?.status
+    if (scenarioStatus === 'FAILED') {
+        await global.page.screenshot({
+            path: `./reports/screenshots/${scenario.pickle.name}.png`
+        })
+    }
+
     await global.page.close()
     await global.context.close()
 })
