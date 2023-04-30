4d5b250101e55f082b1cfe5cac208af02ef32f03
tzero86 <eliasmedina86@gmail.com>
2023-04-29 21:25:47 -0300

    + deepDiff 2 debug obj comparisons & table headers test

--- e2e/src/features/playground/tables.feature
+++ e2e/src/features/playground/tables.feature
@@ -1,10 +1,9 @@
 Feature: As a user I can interact with tables.
 
-
     @dev
     @smoke
     @regression
-    Scenario: As a user I can interact and assert on buttons at index
+    Scenario: As a user I can interact and assert table data
         Given I am on the "home" page
         And I click the "playground" button
         When I am directed to the "playground" page
@@ -20,3 +19,16 @@ Feature: As a user I can interact with tables.
             | 262 | 11  | 24 | 6   |
             | 305 | 3.2 | 67 | 4.3 |
             | 356 | 16  | 49 | 3.9 |
+
+
+    @dev
+    @smoke
+    @regression
+    Scenario: As a user I can interact and assert on table headers
+        Given I am on the "home" page
+        And I click the "playground" button
+        When I am directed to the "playground" page
+        And the headers of the "basic" table should equal the following:
+            |Dessert (100g serving)|Calories|Fat (g)|Carbs (g)|Protein (g)|
+
+
--- e2e/src/step-definitions/assertions/verify-table-value.ts
+++ e2e/src/step-definitions/assertions/verify-table-value.ts
@@ -3,6 +3,7 @@ import { ElementKey } from '../../env/global'
 import { getElementLocator } from '../../support/web-element-helper'
 import { ScenarioWorld } from '../setup/world'
 import { waitFor } from '../../support/wait-for-behavior'
+import * as deepDiff from 'deep-diff'
 
 
 Then(
@@ -14,20 +15,55 @@ Then(
         } = this
 
         console.log(`The ${elementKey} table should ${negate ? 'not ': ''}equal the following: ${dataTable}`)
-
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
-
         const dataBefore = await page.$$eval(elementIdentifier+" tbody tr", (rows) => {
             return rows.map((row) => {
                 const cells = row.querySelectorAll('td')
                 return Array.from(cells).map(cell => cell.textContent)
             })
         })
+        console.log('dataBefore: ', JSON.stringify(dataBefore))
+        console.log('dataTable: ', JSON.stringify(dataTable.raw()))
 
         await waitFor(async () => {
             return JSON.stringify(dataBefore) === JSON.stringify(dataTable.raw()) === !negate
         })
 
+    }
+)
+
+Then(
+    /^the headers of the "([^"]*)" table should( not)? equal the following:$/,
+    async function(this: ScenarioWorld, elementKey: ElementKey, negate: boolean, dataTable: DataTable) {
+        const {
+            screen: { page },
+            globalConfig
+        } = this
+
+        
+        console.log(`The headers of the ${elementKey} table should ${negate ? 'not ': ''}equal the following: ${dataTable}`)
+
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
+        const headersBefore = await page.$$eval(elementIdentifier+" thead", (headers) => {
+            return headers.map((header) => {
+                const headers = header.querySelectorAll('th')
+                return Array.from(headers).map(header => header.textContent?.replace(/[\u200B-\u200D\uFEFF]/g, '').replace(/\s+/g, ' ').trim().normalize('NFC'))
+
+            })
+        })
+        
+        const normalizedDataTable = dataTable.raw()
+        console.log('headersBefore: ', JSON.stringify(headersBefore))
+        console.log('normalizedDataTable: ', JSON.stringify(normalizedDataTable))
+        const differences = deepDiff.diff(headersBefore, normalizedDataTable);
+        console.log('Differences: ', differences);
+
+        await waitFor(async () => {
+           return JSON.stringify(headersBefore) == JSON.stringify(normalizedDataTable) === !negate
+        })
+        
+        
+        
     }
 )
 
