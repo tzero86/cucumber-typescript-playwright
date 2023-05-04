ad095fd411058280ff9a5493e3fcd18052431bf8
tzero86 <eliasmedina86@gmail.com>
2023-05-04 16:34:24 -0300

    Test for profile avatars and count of items

---
+++ e2e/src/features/playground/avatars.feature
@@ -0,0 +1,13 @@
+Feature: As a user I can interact with avatars
+
+    
+    @smoke
+    @regression
+    Scenario: As a user I can interact and assert on avatars
+        Given I am on the "home" page
+        And I click the "playground" button
+        When I am directed to the "playground" page
+        And the "1st" "avatar" should be displayed
+        Then the "2nd" "avatar" should be displayed
+        And the "small avatar" should be displayed
+        And I should see "2" "avatar" displayed
--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -22,3 +22,42 @@ Then(
         })
     }
 )
+
+
+Then(
+    /^the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" "([^"]*)" should( not)? be displayed$/,
+    async function (this: ScenarioWorld, elementPosition: string, elementKey: ElementKey, negate: boolean) {
+
+        const {
+            screen: { page },
+            globalConfig
+        } = this
+
+        console.log(`👁️ The ${elementPosition} ${elementKey} should ${negate ? 'not ': ''}be displayed`)
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
+        const index = Number(elementPosition.match(/\d/g)?.join('')) - 1
+
+        await waitFor(async () => {
+            const isElementVisible = (await page.$(`${elementIdentifier}>>nth=${index}`)) != null
+            return isElementVisible === !negate
+        })
+    }
+)
+
+Then(
+    /^I should( not)? see "(\d*)" "([^"]*)" displayed$/,
+    async function (this: ScenarioWorld, negate: boolean, expectedCount: string, elementKey: ElementKey) {
+        const {
+            screen: {page},
+            globalConfig
+        } = this
+
+        console.log(`👁️ I should ${negate ? 'not ': ''}see ${expectedCount} ${elementKey} displayed`)
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
+
+        await waitFor(async () => {
+            const element = await page.$$(elementIdentifier)
+            return (Number(expectedCount) === element.length) === !negate
+        })
+    }
+)
