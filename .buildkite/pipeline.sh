42f35553ea26a6c414c1faba64c38da04132653e
tzero86 <eliasmedina86@gmail.com>
2023-05-16 15:39:29 -0300

    minor fix

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -9,7 +9,7 @@ PIPELINE=${PIPELINE:-build}
 function build() {
     cat <<EOF
 steps:
-    - name: ":desktop: Automation Suite"
+    - name: ":desktop_computer: Automation Suite"
       command: "./scripts/buildkite-automation.sh"
       artifact_paths:
         - "./e2e/reports/cucumber-html-report.html"
